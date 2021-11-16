2162d1914fa53956ddf50969c6678a5d37f852e4
Bushra <87435687+BushraOnika@users.noreply.github.com>
2021-11-16 14:49:38 +0600

    boooooooooooooooooooooooooooooooooooooooo

--- src/Codewar/HumanReadableTime.java
+++ src/Codewar/HumanReadableTime.java
@@ -38,63 +38,7 @@ public class HumanReadableTime {
 
         return "";
     }
-    public static boolean validParentheses(String parens)
-    {
-        Stack<Character> s= new Stack<>();
-        char x;
-
-        // Traversing the Expression
-        for (int i = 0; i < parens.length(); i++)
-        {
-            if (parens.charAt(i) == '(' || parens.charAt(i) == '['
-                    || parens.charAt(i)== '{')
-            {
-                // Push the element in the stack
-                s.push(parens.charAt(i));
-
-                continue;
-            }
-
-            // IF current current character is not opening
-            // bracket, then it must be closing. So stack
-            // cannot be empty at this point.
-            if (s.empty())
-                return false;
-
-            switch (parens.charAt(i)) {
-                case ')':
-
-                    // Store the top element in a
-                    x = s.pop();
-                    s.pop();
-                    if (x == '{' || x == '[')
-                        return false;
-                    break;
-
-                case '}':
-
-                    // Store the top element in b
-                    x = s.pop();
-                    s.pop();
-                    if (x == '(' || x == '[')
-                        return false;
-                    break;
-
-                case ']':
-
-                    // Store the top element in c
-                    x = s.pop();
-                    s.pop();
-                    if (x == '(' || x == '{')
-                        return false;
-                    break;
-            }
-        }
-
-        // Check Empty Stack
-        return (s.empty());
-    }
-
+   
 
 
     public static void main(String[] args) {
